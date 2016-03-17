396868638c2794c5c9b45f943db51ccac916535d
Fred Stober <stober@cern.ch>
2016-03-17 16:37:35 +0100

    Removed old LoggedProcess

--- packages/grid_control/utils/__init__.py
+++ packages/grid_control/utils/__init__.py
@@ -12,7 +12,7 @@
 #-#  See the License for the specific language governing permissions and
 #-#  limitations under the License.
 
-import os, re, sys, glob, stat, time, Queue, errno, popen2, signal, fnmatch, logging, tarfile, operator, threading
+import os, re, sys, glob, stat, time, Queue, fnmatch, logging, tarfile, operator, threading
 from grid_control.gc_exceptions import InstallationError, RuntimeError, UserError
 from grid_control.utils.parsing import parseBool, parseDict, parseInt, parseList, parseStr, parseTime, parseType, strGuid, strTime, strTimeShort
 from grid_control.utils.thread_tools import TimeoutException, hang_protection
@@ -133,100 +133,6 @@ def getThreadedGenerator(genList): # Combines multiple, threaded generators into
 			listThread.pop()
 		else:
 			yield tmp
-
-class LoggedProcess(object):
-	def __init__(self, cmd, args = '', niceCmd = None, niceArgs = None, shell = True):
-		self.niceCmd = QM(niceCmd, niceCmd, os.path.basename(cmd))
-		self.niceArgs = QM(niceArgs, niceArgs, args)
-		(self.stdout, self.stderr, self.cmd, self.args) = ([], [], cmd, args)
-		self._logger = logging.getLogger('process.%s' % os.path.basename(cmd))
-		self._logger.log(logging.DEBUG1, 'External programm called: %s %s' % (self.niceCmd, self.niceArgs))
-		self.stime = time.time()
-		if shell:
-			self.proc = popen2.Popen3('%s %s' % (cmd, args), True)
-		else:
-			if isinstance(cmd, str):
-				cmd = [cmd]
-			if isinstance(args, str):
-				args = args.split()
-			self.proc = popen2.Popen3( cmd + list(args), True)
-
-	def getOutput(self, wait = False):
-		if wait:
-			self.wait()
-		self.stdout.extend(self.proc.fromchild.readlines())
-		return str.join('', self.stdout)
-
-	def getError(self):
-		self.stderr.extend(self.proc.childerr.readlines())
-		return str.join('', self.stderr)
-
-	def getMessage(self):
-		return self.getOutput() + '\n' + self.getError()
-
-	def kill(self):
-		try:
-			os.kill(self.proc.pid, signal.SIGTERM)
-		except OSError, osError:
-			if osError.errno != errno.ESRCH: # errno.ESRCH: no such process (already dead)
-				raise
-
-	def iter(self):
-		while True:
-			try:
-				line = self.proc.fromchild.readline()
-			except Exception:
-				abort(True)
-				break
-			if not line:
-				break
-			self.stdout.append(line)
-			yield line
-
-	def wait(self, timeout = -1, kill = True):
-		if not timeout > 0:
-			return self.proc.wait()
-		while self.poll() < 0 and timeout > ( time.time() - self.stime ):
-			time.sleep(1)
-		if kill and timeout > ( time.time() - self.stime ):
-			self.kill()
-		return self.poll()
-
-	def poll(self):
-		return self.proc.poll()
-
-	def getAll(self):
-		self.stdout.extend(self.proc.fromchild.readlines())
-		self.stderr.extend(self.proc.childerr.readlines())
-		return (self.wait(), self.stdout, self.stderr)
-
-	def logError(self, target, brief=False, **kwargs): # Can also log content of additional files via kwargs
-		now = time.time()
-		entry = '%s.%s' % (time.strftime('%Y-%m-%d_%H:%M:%S', time.localtime(now)), ('%.5f' % (now - int(now)))[2:])
-		eprint('WARNING: %s failed with code %d' % (self.niceCmd, self.wait()), printTime=True)
-		if not brief:
-			eprint('\n%s' % self.getError(), printTime=True)
-
-		try:
-			tar = tarfile.TarFile.open(target, 'a')
-			data = {'retCode': self.wait(), 'exec': self.cmd, 'args': self.args}
-			files = [VirtualFile(os.path.join(entry, 'info'), DictFormat().format(data))]
-			kwargs.update({'stdout': self.getOutput(), 'stderr': self.getError()})
-			for key, value in kwargs.items():
-				try:
-					content = open(value, 'r').readlines()
-				except Exception:
-					content = [value]
-				files.append(VirtualFile(os.path.join(entry, key), content))
-			for fileObj in files:
-				info, handle = fileObj.getTarInfo()
-				tar.addfile(info, handle)
-				handle.close()
-			tar.close()
-		except Exception:
-			raise RuntimeError('Unable to log errors of external process "%s" to "%s"' % (self.niceCmd, target))
-		eprint('All logfiles were moved to %s' % target)
-
 
 ################################################################
 # Global state functions
